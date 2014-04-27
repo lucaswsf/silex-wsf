c81f71873a38f9b88089dafc70a50a33f919d41f
Lucas <lucas.marot@etu-webschoolfactory.fr>
2014-04-27 22:30:47 +0200

    plus
    encore des choses

---
+++ class/Blog/Controller.php
@@ -0,0 +1,56 @@
+<?php
+namespace Blog;
+
+use PDO;
+
+Class Controller
+{
+    public $app;
+    public $data = array();
+
+    public function __construct($app)
+    {
+        $this->app = $app;
+
+        $this->data['user'] = $this->isLogged();
+        $this->data['admin'] = $this->isAdmin();
+    }
+
+    /**
+     * Renvoi l'user de la session si loggué, sinon false.
+     * @return boolean
+     */
+    public function isLogged()
+    {
+        $user = $this->app['session']->get('user');
+
+        return empty($user) ? false : $user;
+    }
+
+    /**
+     * Vérifie si un user est admin
+     * @return boolean
+     */
+    public function isAdmin()
+    {
+        $user = $this->isLogged();
+
+        $sql = 'SELECT admin FROM users WHERE id= :id';
+        $arg = array(
+            ':id' => $user['id']
+        );
+        $result = $this->app['sql']
+                       ->prepareExec($sql, $arg)
+                       ->fetch(PDO::FETCH_ASSOC);
+       // $result = $result;
+       //
+        return $result['admin'];
+    }
+
+    public function redirect($route)
+    {
+        return $this->app->redirect(
+            $this->app['url_generator']->generate($route)
+        );
+    }
+}
